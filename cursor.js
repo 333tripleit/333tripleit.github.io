af2a11d490c68f58b6339112b55838085444202c
OoonyxxX <89767420+OoonyxxX@users.noreply.github.com>
2025-08-24 19:44:25 +0300

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,53 +1,76 @@
 const customCursor = document.getElementById('custom-cursor');
 const scaleGroup = document.getElementById('scaleSup');
 const customCursorMode   = document.getElementById('toggle-cursor');
-// обновление позиции курсора
+
+const hasTouch = navigator.maxTouchPoints > 0;
+
 let customCursorEnabled = true;
 document.body.classList.add('custom-cursor-on');
 
-document.addEventListener('mousemove', e => {
-  if (customCursorEnabled) {
-    customCursor.style.left = (e.clientX - 26) + 'px';
-    customCursor.style.top  = (e.clientY - 26) + 'px';
+let mouseX = 0, mouseY = 0, ticking = false;
+customCursor.style.opacity = '0';
+
+
+if (!hasTouch) {
+	if (customCursorEnabled) {
+		document.addEventListener('mousemove', handleMouseMove);
+	}
+/*
+	document.addEventListener('mousemove', e => {
+	  if (customCursorEnabled) {
+		customCursor.style.left = (e.clientX - 26) + 'px';
+		customCursor.style.top  = (e.clientY - 26) + 'px';
+	  }
+	});
+*/
+
+	customCursorMode.addEventListener('change', () => {
+	  customCursorEnabled = !customCursorEnabled;
+	  customCursor.style.display = customCursorEnabled ? 'block' : 'none';
+	  document.body.classList.toggle('custom-cursor-on', customCursorEnabled);
+		if (customCursorEnabled) {
+			document.addEventListener('mousemove', handleMouseMove);
+		} else {
+			document.removeEventListener('mousemove', handleMouseMove)
+		}
+	});
+	
+	const activeSelectors = '.leaflet-marker-icon, a, button, .leaflet-popup-close-button';
+
+	document.addEventListener('mouseover', e => {
+	  if (e.target.closest(activeSelectors) && !e.relatedTarget?.closest(activeSelectors)) {
+		customCursor.classList.remove('cursor-base');
+		customCursor.classList.add('cursor-base--hover');
+		scaleGroup.style.transform = 'scale(1)';
+	  }
+	});
+
+	document.addEventListener('mouseout', e => {
+	  if (e.target.closest(activeSelectors) && !e.relatedTarget?.closest(activeSelectors)) {
+		customCursor.classList.remove('cursor-base--hover');
+		customCursor.classList.add('cursor-base');
+		scaleGroup.style.transform = 'scale(0.7)';
+	  }
+	});
+} else {
+	customCursor.style.display = 'none';
+	customCursorMode.addEventListener('change', () => {
+	  customCursorEnabled = !customCursorEnabled;
+	  customCursor.style.display = 'none';
+	});
+}
+
+function handleMouseMove(e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  if (!ticking) {
+	requestAnimationFrame(() => {
+	  if (customCursorEnabled) {
+		customCursor.style.transform = `translate(${mouseX - 26}px, ${mouseY - 26}px)`;
+	  }
+	  ticking = false;
+	  customCursor.style.opacity = '1';
+	});
+	ticking = true;
   }
-});
-
-customCursorMode.addEventListener('change', () => {
-  customCursorEnabled = !customCursorEnabled;
-  customCursor.style.display = customCursorEnabled ? 'block' : 'none';
-  customCursorMode.textContent = customCursorEnabled ? 'Disable Cursor' : 'Enable Cursor';
-  document.body.classList.toggle('custom-cursor-on', customCursorEnabled);
-});
-customCursorMode.textContent = 'Disable Cursor';
-
-// элемент карты из scriptv2.js
-const mapContainer = document.getElementById('map');
-
-// показываем базовый курсор при наведении на карту
-//mapContainer.addEventListener('mouseenter', () => {
-//  customCursor.classList.remove('cursor-base--hover');
-//});
-
-// скрываем кастомный курсор при уходе с карты
-//mapContainer.addEventListener('mouseleave', () => {
-//  customCursor.classList.remove('cursor-base--hover');
-//});
-
-// активные элементы, для которых нужно показывать анимацию hover
-const activeSelectors = '.leaflet-marker-icon, a, button, .leaflet-popup-close-button';
-
-document.addEventListener('mouseover', e => {
-  if (e.target.closest(activeSelectors) && !e.relatedTarget?.closest(activeSelectors)) {
-	customCursor.classList.remove('cursor-base');
-    customCursor.classList.add('cursor-base--hover');
-	scaleGroup.style.transform = 'scale(1)';
-  }
-});
-
-document.addEventListener('mouseout', e => {
-  if (e.target.closest(activeSelectors) && !e.relatedTarget?.closest(activeSelectors)) {
-    customCursor.classList.remove('cursor-base--hover');
-    customCursor.classList.add('cursor-base');
-    scaleGroup.style.transform = 'scale(0.7)';
-  }
-});
+}
